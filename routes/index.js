11e251af0a0ac8379ad25b784f577b3c504804bd
Shi Ling Tai <shiling.tai@gmail.com>
2019-06-11 18:00:35 +0800

    Create pokedex page!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
+const Pokemon = require("../models/Pokemon")
+const Pokedex = require("../db/Pokedex")
+const config = require("../config")[process.env.NODE_ENV || "development"]
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// init database
+let pokedex = new Pokedex()
+pokedex.connect(config.database.filename)
 
-module.exports = router;
+router.get("/", async function(req, res, next) {
+
+	// get all pokemons
+	let filter = {}
+
+	// name
+	if (req.query.name) {
+		filter.name = req.query.name
+	}
+
+	// type
+
+	// generation
+	if (req.query.generation) {
+		filter.generation = Number.parseInt(req.query.generation)
+	}
+
+	// legendary
+	if (req.query.legendary) {
+		filter.legendary = req.query.legendary === "1"
+	}
+
+	let pokemons = await pokedex.find({filter: filter})
+
+	res.render("index", {
+		title: "Pokedex",
+		pokemons: pokemons
+	})
+
+})
+
+module.exports = router
